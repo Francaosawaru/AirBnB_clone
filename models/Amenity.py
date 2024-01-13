a202e844595fae9d493a111f1ef270ae2474cdf0
Samosky9 <haryanwolesamuel27@gmail.com>
2024-01-13 15:46:30 -0800

    models

---
+++ models/Amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Amenities class that inherits from the baseModel """
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ city subclass of BaseModel """
+
+name = ""
+
